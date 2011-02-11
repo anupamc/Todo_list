d42ddbb25337c98f4831d76f5eafd7b3743d4ca3
Anupam Choudhury <achoudhury@crri.co.in>
2011-02-11 12:09:44 +0600

    Created the backbone of the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoList::Application.config.secret_token = 'ba8959fb53229f39a1b264f6e6d6f0f1aaf29390ba48cba9b8b7ff9fe9e903674f9752b9bb5434ae87f06b62098e75a310ad218fa0526a348aac3864e62174a9'
